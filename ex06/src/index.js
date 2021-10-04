c5c29a0cd19b9e46bbc4b87e7089b2701268c750
timurseta <timur_seta@hotmail.com>
2021-10-04 10:33:33 +0200

    ex06 added

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+// only change code below this line
+let rockStar = new Map([
+    ['artist', 'The Rolling Stones'],
+    ['song', 'Angie'],
+    ['album', 'Goats Head Soup'],
+    ['singer', 'Mick Jagger']
+]);
+
+let newRock = new Map();
+for(let key of rockStar.entries()){
+    if(!key[0].includes('a')){
+        newRock.set(`${key[0]}`,`${key[1]}`);
+    }
+}
+
+console.log(newRock);
+// only change code above this line
+
+module.exports = { rockStar, newRock };
